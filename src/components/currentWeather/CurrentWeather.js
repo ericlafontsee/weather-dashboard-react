5b1d7f51701bdf40edb0957a2224b9fdf945deab
Eric LaFontsee <elafontsee@gmail.com>
2023-11-16 05:45:15 -0600

    added a fetcCoordinates function to geocode a user's entered zip code

--- src/components/currentWeather/CurrentWeather.js
+++ src/components/currentWeather/CurrentWeather.js
@@ -2,20 +2,20 @@ import React from "react";
 
 const CurrentWeather = () => {
   return (
-    <div class="currentWeather__container">
-      <h2 id="currentCity" class="text-dark p-3">
+    <div className="currentWeather__container">
+      <h2 id="currentCity" className="text-dark p-3">
         Today's Forecast
       </h2>
-      <p class="text-dark p-3">
+      <p className="text-dark p-3">
         Temperature: <span id="temperature"></span>
       </p>
-      <p class="text-dark p-3">
+      <p className="text-dark p-3">
         Humidity: <span id="humidity"></span>
       </p>
-      <p class="text-dark p-3">
+      <p className="text-dark p-3">
         Wind Speed: <span id="windSpeed"></span>
       </p>
-      <p class="text-dark p-3">
+      <p className="text-dark p-3">
         UV Index: <span id="uvIndexSpan"></span>
       </p>
     </div>
--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,30 +2,40 @@ import { React, useState, useEffect } from "react";
 import "./Search.css";
 
 const Search = () => {
-  const [city, setCity] = useState("");
+  const [zip, setZip] = useState("");
+  const [lat, setLat] = useState("");
+  const [lon, setLon] = useState("");
   const [weather, setWeather] = useState([]);
-
-
-  /* NOTE TO SELF - GEOCODER HAS BEEN DEPRECATED, USE GEOCODER API SUGGESTED TO CONVERT CITY INTO LAT AND LONG */
 
   // Click event on search button will populate page with input city's weather
   const submitHandler = (event) => {
     event.preventDefault();
 
-    if (city === "") {
-      alert("Enter a city!");
+    if (zip === "") {
+      alert("Enter a zip code!");
     } else {
-      fetchForecast(city);
+      fetchCoordinates(zip);
     }
   };
 
-  const cityChangeHandler = (event) => {
-    setCity(event.target.value);
+  const zipChangeHandler = (event) => {
+    setZip(event.target.value);
   };
 
-  const fetchForecast = (city) => {
+  const fetchCoordinates = (zip) => {
     var APIKey = "205e65f3bbbf65a0b6de249409614a50";
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=${APIKey}`;
+    var queryURL = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=${APIKey}`;
+    fetch(queryURL)
+      .then((response) => response.json())
+      .then((data) => {
+        setLat(data.lat);
+        setLon(data.lon);
+      });
+  };
+
+  const fetchForecast = (lat, lon) => {
+    var APIKey = "205e65f3bbbf65a0b6de249409614a50";
+    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}.34&lon=${lon}&appid=${APIKey}`;
     fetch(queryURL)
       .then(response => response.json())
       .then(data => setWeather(data));
@@ -33,20 +43,25 @@ const Search = () => {
   };
 
   useEffect(() => {
+    // This block will be executed after the state has been updated
+    console.log("LAT: ", lat);
+    console.log("LON: ", lon);
+  }, [lat, lon]);
+
+  useEffect(() => {
     fetchForecast();
   }, []);
-
 
   return (
     <div className="search__container">
       <h2>Search City: </h2>
       <form onSubmit={submitHandler}>
         <input
-          id="citySearch"
+          id="zipSearch"
           type="text"
-          placeholder="Enter City"
-          value={city}
-          onChange={cityChangeHandler}
+          placeholder="Enter Zip Code"
+          value={zip}
+          onChange={zipChangeHandler}
         />
         <button id="searchBtn" type="submit" class="">
           <i class="fas fa-search-location"></i>
