2baebbf9661b120f63d497974a6144dbedd247d3
Eric LaFontsee <elafontsee@gmail.com>
2023-11-29 06:23:33 -0600

    added useEffect

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -6,16 +6,26 @@ const Search = () => {
   const [lat, setLat] = useState("");
   const [lon, setLon] = useState("");
   const [weather, setWeather] = useState([]);
+  const [temp, setTemp] = useState([]);
+
+  useEffect(() => {
+    // This block will be executed after the state has been updated
+    fetchForecast();
+  }, [lat, lon]);
+
+  useEffect(() => {
+    // This block will be executed after the state has been updated
+    // console.log(weather);
+    // console.log(temp);
+    // let tempF = (weather.list[0].main.temp - 273.15) * 1.80 + 32;
+    // let tempF = weather.list[0].main.temp;
+    // console.log(tempF);
+  }, [weather]);
 
   // Click event on search button will populate page with input city's weather
   const submitHandler = (event) => {
     event.preventDefault();
-
-    if (zip === "") {
-      alert("Enter a zip code!");
-    } else {
-      fetchCoordinates(zip);
-    }
+    zip === "" ? alert("Enter a zip code!") : fetchCoordinates(zip);
   };
 
   const zipChangeHandler = (event) => {
@@ -23,8 +33,9 @@ const Search = () => {
   };
 
   const fetchCoordinates = (zip) => {
-    var APIKey = "205e65f3bbbf65a0b6de249409614a50";
-    var queryURL = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=${APIKey}`;
+    let APIKey = "205e65f3bbbf65a0b6de249409614a50";
+    let queryURL = `http://api.openweathermap.org/geo/1.0/zip?zip=${zip}&appid=${APIKey}`;
+
     fetch(queryURL)
       .then((response) => response.json())
       .then((data) => {
@@ -33,24 +44,21 @@ const Search = () => {
       });
   };
 
-  const fetchForecast = (lat, lon) => {
-    var APIKey = "205e65f3bbbf65a0b6de249409614a50";
-    var queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}.34&lon=${lon}&appid=${APIKey}`;
+  const fetchForecast = () => {
+    let APIKey = "205e65f3bbbf65a0b6de249409614a50";
+    let queryURL = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${APIKey}`;
+
     fetch(queryURL)
-      .then(response => response.json())
-      .then(data => setWeather(data));
-    console.log("WEATHER: ", weather);
+      .then((response) => response.json())
+      .then((data) => {
+        setWeather(data);
+        if (data.list && data.list.length > 0) {
+          if (data.list[0].main) {
+            console.log("DATA: ", data.list[0].main.temp);
+          }
+        }
+      });
   };
-
-  useEffect(() => {
-    // This block will be executed after the state has been updated
-    console.log("LAT: ", lat);
-    console.log("LON: ", lon);
-  }, [lat, lon]);
-
-  useEffect(() => {
-    fetchForecast();
-  }, []);
 
   return (
     <div className="search__container">
